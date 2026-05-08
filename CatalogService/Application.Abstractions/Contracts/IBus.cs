f1f44f31c13117c3e5f44c11e784ab1817c9d786
Mohamad <Mohamadshirzad81@gmail.com>
2026-05-09 01:15:48 +0330

    Restructure repository layout

--- CatalogService/Application.Abstractions/Contracts/IBus.cs
+++ CatalogService/Application.Abstractions/Contracts/IBus.cs
@@ -2,15 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Domain.Abstractions;
 
 namespace Application.Abstractions.Contracts
 {
    public interface IBus
 {
     Task PublishAsync<T>(T message, CancellationToken cancellationToken = default)
-        where T : class;
+        where T : IDomainEvent;
 
-        Task PublishIntegratedMessage<T>(T message, CancellationToken cancellationToken = default)
+        Task PublishIntegrationMessage<T>(T message, CancellationToken cancellationToken = default)
         where T : class;
 }
 
--- CatalogService/CatalogService.Infrastructure/Bus.cs
+++ CatalogService/CatalogService.Infrastructure/Bus.cs
@@ -4,18 +4,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using MediatR;
 using Application.Abstractions.Contracts;
+using Domain.Abstractions;
 
 namespace CatalogService.Infrastructure
 {
     public class Bus(IMediator mediator): IBus
     {
-        public async Task PublishAsync<T>(T message, CancellationToken cancellationToken = default) where T : class
+        public async Task PublishAsync<T>(T message, CancellationToken cancellationToken = default)
+          where T : IDomainEvent
+         
         {
         await   mediator.Publish(message, cancellationToken);
       
         }
 
-        public Task PublishIntegratedMessage<T>(T message, CancellationToken cancellationToken = default) where T : class
+        public Task PublishIntegrationMessage<T>(T message, CancellationToken cancellationToken = default)
+         where T : class
         {
             throw new NotImplementedException();
         }
